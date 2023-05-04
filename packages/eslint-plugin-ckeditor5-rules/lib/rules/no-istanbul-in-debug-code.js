d55fe0ed37574e8194541976274c3136e694b46f
Michał Remiszewski <m.remiszewski@cksource.com>
2023-05-04 10:05:25 +0200

    Add rule to prevent vite breaks.

---
+++ packages/eslint-plugin-ckeditor5-rules/lib/rules/no-istanbul-in-debug-code.js
@@ -0,0 +1,36 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+module.exports = {
+	meta: {
+		type: 'problem',
+		docs: {
+			description: 'Enforce CK_DEBUG and instanbul not appearing in the same line.',
+			category: 'CKEditor5'
+		}
+	},
+
+	create( context ) {
+		const sourceCode = context.getSourceCode();
+
+		return {
+			Program( node ) {
+				let comments = sourceCode.getComments( node.body[ 0 ] || node );
+				comments = [ ...comments.leading, ...comments.trailing ];
+
+				for ( const comment of comments ) {
+					if ( comment.value.includes( 'CK_DEBUG' ) && comment.value.includes( 'istanbul' ) ) {
+						context.report( {
+							node: comment,
+							message: 'Comments cannot have both CK_DEBUG and istanbul flags.'
+						} );
+					}
+				}
+			}
+		};
+	}
+};
