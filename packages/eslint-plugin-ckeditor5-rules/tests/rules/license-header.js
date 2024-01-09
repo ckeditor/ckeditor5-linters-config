7db2b616f5f19ab05e0d317aff14798495c3dd03
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-01-09 10:44:16 +0100

    Internal: Bumped the year.

--- packages/eslint-plugin-ckeditor5-rules/tests/rules/license-header.js
+++ packages/eslint-plugin-ckeditor5-rules/tests/rules/license-header.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md.
  */
 
@@ -17,7 +17,7 @@ const missingHeaderError = { message: 'The license header is missing.' };
 const options = [ {
 	headerLines: [
 		'/**',
-		' * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.',
+		' * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.',
 		' * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license',
 		' */'
 	]
