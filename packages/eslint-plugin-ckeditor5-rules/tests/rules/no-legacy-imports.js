57e0af6a75fc349484f7a568736f79ea45aa7778
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-09-04 14:41:58 +0200

    Create fake package.json to make tests green.

--- packages/eslint-plugin-ckeditor5-rules/tests/rules/no-legacy-imports.js
+++ packages/eslint-plugin-ckeditor5-rules/tests/rules/no-legacy-imports.js
@@ -6,12 +6,52 @@
 'use strict';
 
 const RuleTester = require( 'eslint' ).RuleTester;
+const rule = require( '../../lib/rules/no-legacy-imports' );
+const fs = require( 'fs-extra' );
+const upath = require( 'upath' );
+
+function mockPackageJson( config ) {
+	const packagesToRemove = [];
+
+	for ( const [ location, content ] of Object.entries( config ) ) {
+		const pkgJsonPath = upath.join( 'node_modules', location );
+
+		if ( fs.pathExistsSync( pkgJsonPath ) ) {
+			console.warn( `Cannot mock the "${ location }" file as it already exists.` );
+			continue;
+		}
+
+		fs.ensureFileSync( pkgJsonPath );
+		fs.writeFileSync( pkgJsonPath, content );
+		packagesToRemove.push( pkgJsonPath );
+	}
+
+	return () => {
+		for ( const item of packagesToRemove ) {
+			fs.removeSync( upath.join( 'node_modules', item.split( '/' )[ 0 ] ) );
+		}
+	};
+}
+
+// Create fake `package.json` files to avoid installing real dependencies.
+const removeMock = mockPackageJson( {
+	'ckeditor5/package.json': JSON.stringify( {
+		dependencies: {
+			'@ckeditor/ckeditor5-core': '*'
+		}
+	} ),
+	'ckeditor5-premium-features/package.json': JSON.stringify( {
+		dependencies: {
+			'@ckeditor/ckeditor5-ai': '*'
+		}
+	} )
+} );
 
 const ruleTester = new RuleTester( {
 	parser: require.resolve( '@typescript-eslint/parser' )
 } );
 
-ruleTester.run( 'no-legacy-imports', require( '../../lib/rules/no-legacy-imports' ), {
+ruleTester.run( 'no-legacy-imports', rule, {
 	valid: [
 
 		// Default options - fix all CKEditor5 imports.
@@ -100,3 +140,5 @@ ruleTester.run( 'no-legacy-imports', require( '../../lib/rules/no-legacy-imports
 		}
 	]
 } );
+
+removeMock();
