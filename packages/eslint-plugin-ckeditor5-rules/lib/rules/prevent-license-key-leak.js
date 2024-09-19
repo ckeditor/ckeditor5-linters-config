8840c209fd8e6e716eeaf856c08ab3eb50bc66ec
Filip Sobol <filipsobol@users.noreply.github.com>
2024-09-19 10:04:57 +0200

    Add `prevent-license-key-leak` rule (#39)
    Feature (eslint-plugin-ckeditor5-rules): Add the `prevent-license-key-leak` rule.

---
+++ packages/eslint-plugin-ckeditor5-rules/lib/rules/prevent-license-key-leak.js
@@ -0,0 +1,49 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+const ALLOWED = [
+	'<YOUR_LICENSE_KEY>',
+	'GPL'
+];
+
+module.exports = {
+	meta: {
+		type: 'problem',
+		docs: {
+			description: 'Prevent leaking of the license keys',
+			category: 'CKEditor5'
+		},
+		fixable: 'code',
+		messages: {
+			licenseKeyLeak: `This code contains a license key. Replace it with the "${ ALLOWED[ 0 ] }" string.`
+		}
+	},
+	create( context ) {
+		return {
+			ObjectExpression( node ) {
+				const property = node.properties.find( property => {
+					return property.type === 'Property' &&
+						property.key.name === 'licenseKey' &&
+						property.value.type === 'Literal' &&
+						!ALLOWED.includes( property.value.value );
+				} );
+
+				if ( !property ) {
+					return;
+				}
+
+				const quote = property.value.raw[ 0 ];
+
+				context.report( {
+					node: property,
+					messageId: 'licenseKeyLeak',
+					fix: fixer => fixer.replaceText( property.value, quote + ALLOWED[ 0 ] + quote )
+				} );
+			}
+		};
+	}
+};
