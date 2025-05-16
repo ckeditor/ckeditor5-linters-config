a1c1a0a0b5650700923250d9f36794cc08d40267
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-05-16 17:28:20 +0200

    Removed unnecesary rules.

--- packages/eslint-config-ckeditor5/eslint.config.mjs
+++ packages/eslint-config-ckeditor5/eslint.config.mjs
@@ -292,7 +292,7 @@ const rulesTypeScript = [
 			}
 		},
 
-		extends: [ 'ts/recommendedTypeChecked' ],
+		extends: [ 'ts/recommended' ],
 
 		files: [ '**/*.@(ts|tsx)' ],
 
@@ -307,45 +307,7 @@ const rulesTypeScript = [
 				default: 'generic'
 			} ],
 
-			'@typescript-eslint/await-thenable': 'off',
-
-			'@typescript-eslint/no-base-to-string': 'off',
-
-			'@typescript-eslint/no-duplicate-type-constituents': 'off',
-
-			'@typescript-eslint/no-floating-promises': 'off',
-
-			'@typescript-eslint/no-misused-promises': 'off',
-
-			'@typescript-eslint/no-redundant-type-constituents': 'off',
-
-			'@typescript-eslint/no-unnecessary-type-assertion': 'off',
-
-			'@typescript-eslint/no-unsafe-argument': 'off',
-
-			'@typescript-eslint/no-unsafe-assignment': 'off',
-
-			'@typescript-eslint/no-unsafe-call': 'off',
-
 			'@typescript-eslint/no-unsafe-function-type': 'off',
-
-			'@typescript-eslint/no-unsafe-member-access': 'off',
-
-			'@typescript-eslint/no-unsafe-return': 'off',
-
-			'@typescript-eslint/only-throw-error': 'off',
-
-			'@typescript-eslint/prefer-promise-reject-errors': 'off',
-
-			'@typescript-eslint/require-await': 'off',
-
-			'@typescript-eslint/restrict-plus-operands': 'off',
-
-			'@typescript-eslint/restrict-template-expressions': 'off',
-
-			'@typescript-eslint/unbound-method': 'off',
-
-			'@typescript-eslint/no-for-in-array': 'off',
 
 			'@typescript-eslint/no-empty-object-type': 'error',
 
