743ce80499c4a11ef1e503c5daef1ee4c43c0e58
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-07-10 13:38:13 +0200

    Added index file.

---
+++ packages/stylelint-plugin-ckeditor5-rules/lib/index.mjs
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import ckContentVariableName from './ck-content-variable-name.mjs';
+import licenseHeader from './license-header.mjs';
+
+export default [
+	ckContentVariableName,
+	licenseHeader
+];
