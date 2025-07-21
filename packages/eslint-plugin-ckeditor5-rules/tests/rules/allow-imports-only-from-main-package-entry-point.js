21d4ec96080567eb6af840506821963af8112c78
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-07-21 17:53:03 +0200

    Added fixer.

--- packages/eslint-plugin-ckeditor5-rules/tests/rules/allow-imports-only-from-main-package-entry-point.js
+++ packages/eslint-plugin-ckeditor5-rules/tests/rules/allow-imports-only-from-main-package-entry-point.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+const message = 'Importing from "@ckeditor/*" packages is only allowed from the main package entry point.';
+
 const RuleTester = require( 'eslint' ).RuleTester;
 
 const ruleTester = new RuleTester( {
@@ -19,35 +21,33 @@ ruleTester.run(
 	require( '../../lib/rules/allow-imports-only-from-main-package-entry-point.js' ),
 	{
 		valid: [
-			'import { Table } from "@ckeditor/ckeditor5-table";'
+			'import Foo from \'Foo\';',
+			'import Foo from \'ckeditor5-foo/bar/baz.js\';',
+			'import { Table } from \'@ckeditor/ckeditor5-table\';'
 		],
 		invalid: [
 			// Do not allow importing from the `src` folder.
 			{
 				code: 'import Table from "@ckeditor/ckeditor5-table/src/table";',
-				errors: [
-					{
-						message: 'Importing from "@ckeditor/*" packages is only allowed from the main package entry point.'
-					}
-				]
+				output: 'import { Table } from \'@ckeditor/ckeditor5-table\';',
+				errors: [ { message } ]
 			},
 			// Do not allow importing icons from the `theme` folder.
 			{
 				code: 'import icon from "@ckeditor/ckeditor5-table/theme/icons/icon.svg";',
-				errors: [
-					{
-						message: 'Importing from "@ckeditor/*" packages is only allowed from the main package entry point.'
-					}
-				]
+				output: 'import { icon } from \'@ckeditor/ckeditor5-table\';',
+				errors: [ { message } ]
 			},
 			// Do not allow importing style sheets from the `theme` folder.
 			{
 				code: 'import styles from "@ckeditor/ckeditor5-table/theme/styles.css";',
-				errors: [
-					{
-						message: 'Importing from "@ckeditor/*" packages is only allowed from the main package entry point.'
-					}
-				]
+				output: 'import { styles } from \'@ckeditor/ckeditor5-table\';',
+				errors: [ { message } ]
+			},
+			// Do not fix if there are both default and named imports.
+			{
+				code: 'import Foo, { Bar } from "@ckeditor/ckeditor5-core/src/core";',
+				errors: [ { message } ]
 			}
 		]
 	}
